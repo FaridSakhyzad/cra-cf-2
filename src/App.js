d34674dc4f4348a3b79dd5a30142de3d69604d1a
Farid Sakhizad <fsakhyzadcf@gmail.com>
2024-08-14 12:09:35 -0600

    CRA | React Lazy Implementation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, } from 'react-router-dom';
+import React, { Suspense } from 'react';
 
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import About from "./pages/About";
-import Faq from "./pages/Faq";
+const Home = React.lazy(() => import('./pages/Home'));
+const Profile = React.lazy(() => import('./pages/Profile'));
+const About = React.lazy(() => import('./pages/About'));
+const Faq = React.lazy(() => import('./pages/Faq'));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/faq" element={<Faq />} />
-    </Routes>
+    <Suspense fallback={<div>Loading</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<Faq />} />
+      </Routes>
+    </Suspense>
   );
 }
 
